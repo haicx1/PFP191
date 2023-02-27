6238f1472c9bd508c333fe4106804942ae7a845b
Haicx1 <haicx981@gmail.com>
2023-02-27 19:38:36 +0700

    Update

--- Pupil_Menu.py
+++ Pupil_Menu.py
@@ -16,55 +16,55 @@ class Student:
 
     @property
     def id(self):
-        return self._id
+        return self.id
 
     @property
     def name(self):
-        return self._name
+        return self.name
 
     @name.setter
-    def name(self, name):
-        self._name = name
+    def name(self, value):
+        self.name = value
 
     @property
     def mark_e(self):
         return self.mark_e
 
     @mark_e.setter
-    def mark_e(self, mark_e):
-        self._mark_e = mark_e
+    def mark_e(self, value):
+        self.mark_e = value
 
     @property
     def mark_m(self):
-        return self._mark_m
+        return self.mark_m
 
     @mark_m.setter
-    def mark_m(self, mark_m):
-        self._mark_m = mark_m
+    def mark_m(self, value):
+        self.mark_m = value
 
     @property
     def mark_p(self):
         return self.mark_p
 
     @mark_p.setter
-    def mark_p(self, mark_p):
-        self._mark_p = mark_p
+    def mark_p(self, value):
+        self._mark_p = value
 
     @property
     def mark_chem(self):
-        return self._mark_chem
+        return self.mark_chem
 
     @mark_chem.setter
-    def mark_chem(self, mark_chem):
-        self._mark_chem = mark_chem
+    def mark_chem(self, value):
+        self.mark_chem = value
 
     @property
     def mark_cs(self):
-        return self._mark_cs
+        return self.mark_cs
 
     @mark_cs.setter
-    def mark_cs(self, mark_cs):
-        self._mark_cs = mark_cs
+    def mark_cs(self, value):
+        self.mark_cs = value
 
     def display(self):
         print('Roll number:', self._id)
@@ -77,12 +77,18 @@ class Student:
 
 
 stuList = list()
-search = False
 
 
-def createStudent(arr):
-    student = Student()
-    arr.append(student)
+def create_student(arr):
+    student1 = Student()
+    arr.append(student1)
+    while True:
+        choice_1 = input('Wants to enter more record (y/n):')
+        if choice_1 == 'y' or choice_1 == 'Y':
+            create_student(stuList)
+            break
+        elif choice_1 == 'n' or choice_1 == 'N':
+            break
     return arr
 
 
@@ -102,32 +108,96 @@ def printMenu1():
     print('2. ADMIN MENU')
     print('3. EXIT')
 
-def mod_student(roll_number):
-    name = stuList[roll_number][1]
-    e = stuList[roll_number][2]
-    m = stuList[roll_number][3]
-    p = stuList[roll_number][4]
-    chem = stuList[roll_number][5]
-    cs = stuList[roll_number][6]
-    print('Name:',name)
+
+def mod_student1(student):
+    print('Name:', student._name)
     choice = input('Wants to edit(y/n)?')
     if choice == 'y' or choice == 'Y':
-        name = input('Enter the name:')
-    elif choice == 'n' or choice == 'N':
-        print('Eng:',e)
-        choice = input('Wants to edit(y/n)?')
-        if choice == 'y' or choice == 'Y':
-            e = input('English marks:')
-        elif choice == 'n' or choice == 'N':
-            return 0
+        student._name = input('Enter the name:')
+    print('English marks:', student._mark_e)
+    choice_e = input('Wants to edit(y/n)?')
+    if choice_e == 'y' or choice_e == 'Y':
+        student._mark_e = int(input('Enter mark:'))
+    print('Math marks:', student._mark_m)
+    choice_m = input('Wants to edit(y/n)?')
+    if choice_m == 'y' or choice_m == 'Y':
+        student._mark_m = int(input('Enter mark:'))
+    print('Physics marks:', student._mark_p)
+    choice_p = input('Wants to edit(y/n)?')
+    if choice_p == 'y' or choice_p == 'Y':
+        student._mark_p = int(input('Enter mark:'))
+    print('Chemistry marks:', student._mark_chem)
+    choice_ch = input('Wants to edit(y/n)?')
+    if choice_ch == 'y' or choice_ch == 'Y':
+        student._mark_ch = int(input('Enter mark:'))
+    print('CS marks:', student._mark_cs)
+    choice_cs = input('Wants to edit(y/n)?')
+    if choice_cs == 'y' or choice_cs == 'Y':
+        student._mark_cs = int(input('Enter mark:'))
+    student.display()
 
-def mod_student1(student = Student()):
-    print('Name:', student.name)
+
+def search_student(rollnumber):
+    search = False
+    for student in stuList:
+        if student._id == rollnumber:
+            print('PUPIL DETAIL..')
+            student.display()
+            return student
+            break
+        if not search:
+            print('No result')
+
+def del_student(rollnumber):
+    delete = False
+    for student in stuList:
+        if student._id == rollnumber:
+            stuList.remove(student)
+            delete = True
+        print('record found and deleted')
+    if not delete:
+        print('rollnumber not found')
 
 
 
+def main_menu():
+    printMenu1()
+    while True:
+        try:
+            choice_main = int(input('Enter choice(1-3):'))
+        except:
+            print('invalid input')
+        if choice_main == 1:
+            report_menu()
+        elif choice_main == 2:
+            return True
+            break
+        elif choice_main == 3:
+            return False
+            break
 
 
+def report_menu():
+    print('REPORT MENU')
+    print('1. CLASS RESULT')
+    print('2. PUPIL REPORT CARD')
+    print('3. BACK TO MAIN MENU')
+    while True:
+        try:
+            choice_report = int(input('Enter choice(1-4):'))
+        except:
+            print('error')
+        if choice_report == 1:
+            print('Rollno   Name       English  Maths  Physics  Chemistry  CS')
+            print(stuList[-1])
+            continue
+        if choice_report == 2:
+            roll = int(input('Enter the rollno you want to search:'))
+            search_student(roll)
+            continue
+        if choice_report == 3:
+            break
+            main_menu()
 
 
 while True:
@@ -139,55 +209,34 @@ while True:
     if choice not in range(1, 7):
         continue
     elif choice == 1:
-        createStudent(stuList)
-        while True:
-            choice_1 = input('Wants to enter more record (y/n):')
-            if choice_1 == 'y' or choice_1 == 'Y':
-                createStudent(stuList)
-                continue
-            elif choice_1 == 'n' or choice_1 == 'N':
-                break
+        create_student(stuList)
         continue
     elif choice == 2:
         for student in stuList:
             student.display()
-        choice2 = input('Do you want to continue the program ?(y/n)')
-        if choice2 == 'y' or 'Y':
-            continue
-        elif choice2 == 'n' or 'N':
-            print('Exit program')
-            break
+        continue
     elif choice == 3:
         roll = int(input('Enter rollno you want to search:'))
-        for student in stuList:
-            if student.id() == roll:
-                print('PUPIL DETAIL..')
-                student.display()
-                search = True
-                break
-        if search == False:
-            print('No result')
-        printMenu1()
-        while True:
-            try:
-                choice1 = int(input('Enter choice(1-3):'))
-            except:
-                print('Invalid input')
-            if choice > 3:
-                continue
-            else:
-                break
-        if choice1 == 3:
-            print('EXIT PROGRAM')
-            break
-        if choice1 == 1:
-            continue
+        search_student(roll)
+        main_menu()
     elif choice == 4:
         print('MODIFY RECORD')
         roll_4 = int(input('Enter roll number:'))
-        for student in stuList:
-            if student._id == roll_4:
-                modify_stu = student
-                break
-        print('Name:', student.name)
-        print()
+        mod_student1(stuList[roll_4])
+        stuList[roll_4].display()
+        continue
+    elif choice == 5:
+        print('DELETE RECORD')
+        del_number = int(input('Enter roll number'))
+        del_student(del_number)
+        continue
+    elif choice == 6:
+        isRunning = True
+        print('Back to main menu')
+        isRunning = main_menu()
+        if isRunning:
+            continue
+        else:
+            print('EXITING')
+            break
+
